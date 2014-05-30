c14187576dc5dab4cbfffdb1f387f486fdf4834f
Mark Wainwright <mark@unbounce.com>
2014-05-29 17:24:44 -0700

    Check with Intercom API that the user exists

--- app.js
+++ app.js
@@ -2,19 +2,48 @@
 
   return {
 
-    events: {
-      'app.activated': 'showButton'
+    requests: {
+
+    getUser: function () {
+      return {
+        url: 'https://{{setting.intercomAppID}}:{{setting.intercomAPIKey}}@api.intercom.io/v1/users/?email=' + this.ticket().requester().email(),
+        type: 'GET',
+        dataType: 'json',
+        secure: true
+      };
+    }
+
     },
 
-    showButton: function() {
-      var user = this.currentUser();
-      this.switchTo('button', {
-        intercomAppID: this.setting('intercomAppID'),
-        email: this.ticket().requester().email(),
-        name: this.ticket().requester().name()
-      });
+    events: {
+      'app.activated': function() {
+        // Show the default button
+        this.switchTo('button', {
+          link: 'https://www.intercom.io/apps/' + this.setting('intercomAppID') + '/users/show?email=' + this.ticket().requester().email(),
+          name: this.ticket().requester().name()
+        });
 
-    }
+        // Make the API call (debug)
+        this.ajax('getUser');
+      },
+
+      'getUser.done': function(data) {
+        if (!data.user_id) return false;
+        // Show the button
+        this.switchTo('button', {
+          link: 'https://www.intercom.io/apps/' + this.setting('intercomAppID') + '/users/show?user_id=' + data.user_id,
+          name: data.name
+        });
+      },
+
+      'getUser.fail': function() {
+        // Show the 'no account' message
+        this.switchTo('no-account', {
+          link: 'https://www.intercom.io/a/apps/' + this.setting('intercomAppID') + '/users/segments/active',
+          email: this.ticket().requester().email()
+        });
+      }
+    },
 
   };
 
