c5f716827eaccb0c6e45b7a2bbe41f3a54820f3c
Mark Wainwright <mark@unbounce.com>
2014-05-30 14:53:29 -0700

    Add display of Intercom metadata

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 (function() {
 
+  String.prototype.toTitleCase = function(){
+    var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
+
+    return this.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
+      if (index > 0 && index + match.length !== title.length &&
+        match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
+        (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
+        title.charAt(index - 1).search(/[^\s-]/) < 0) {
+        return match.toLowerCase();
+      }
+
+      if (match.substr(1).search(/[A-Z]|\../) > -1) {
+        return match;
+      }
+
+      return match.charAt(0).toUpperCase() + match.substr(1);
+    });
+  };
+
   return {
 
     requests: {
@@ -18,9 +37,10 @@
     events: {
       'app.activated': function() {
         // Show the default button
-        this.switchTo('button', {
+        this.switchTo('account', {
           link: 'https://app.intercom.io/apps/' + this.setting('intercomAppID') + '/users/show?email=' + this.ticket().requester().email(),
-          name: this.ticket().requester().name()
+          name: this.ticket().requester().name(),
+          metadata: []
         });
 
         // Make the API call (debug)
@@ -29,10 +49,38 @@
 
       'getUser.done': function(data) {
         if (!data.user_id) return false;
-        // Show the button
-        this.switchTo('button', {
+        console.log(data);
+
+        // Metadata fields to grab from Intercom
+        var fields = ['marketer', 'pages', 'domains', 'clients', 'api keys',
+          'session count'];
+
+        // Store those fields in array
+        var metadata = [];
+        for ( var i = 0 ; i < fields.length ; i ++ ){
+          if ( data.custom_data[ fields[i] ] || data.custom_data[ fields[i] ] === 0 ) {
+
+            // Custom data displays
+            if ( fields[i] === 'pages' ) {
+              data.custom_data[ fields[i] ] = data.custom_data[ fields[i] ] + ' (' + data.custom_data['published pages'] + ' published)';
+            }
+
+            if ( fields[i] === 'marketer' ) {
+              data.custom_data[ fields[i] ] = data.custom_data[ fields[i] ].toTitleCase();
+            }
+
+            metadata.push({
+              field : fields[i].toTitleCase(),
+              data : data.custom_data[ fields[i] ]
+            });
+          }
+        }
+
+        // Show the button and data
+        this.switchTo('account', {
           link: 'https://app.intercom.io/apps/' + this.setting('intercomAppID') + '/users/show?user_id=' + data.user_id,
-          name: data.name
+          name: data.name,
+          metadata: metadata
         });
       },
 
